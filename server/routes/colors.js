d4a283dd1939bb4c6452af2069e2baf44e8f155f
Prechyious <lemoprechy@gmail.com>
2023-05-03 15:40:34 +0100

    finished practice

---
+++ server/routes/colors.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json("GET /colors");
+});
+
+router.get("/:name", (req, res) => {
+    const name = req.params.name;
+
+    res.json(`GET /colors/:name`);
+});
+
+router.post("/:name/css-styles", (req, res) => {
+    const name = req.params.name;
+
+    res.json(`POST /colors/:name/css-styles`);
+});
+
+router.delete("/:name/css-styles/:style", (req, res) => {
+    const name = req.params.name;
+    const style = req.params.style;
+
+    res.json(`DELETE /colors/:name/css-styles/:style`);
+})
+
+module.exports = router;
